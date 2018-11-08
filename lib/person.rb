677d98ed415fc23c66b15d8c8c379e4cc3cdbfea
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-08 18:36:39 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+def Person
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name 
+    @name
+  end
+  
+  def job=(occupation)
+    @job = occupation
+  end
+  
+  def job
+    @job 
+  end
+end
