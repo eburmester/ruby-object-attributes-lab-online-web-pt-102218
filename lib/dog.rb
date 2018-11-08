0e8500860948ec05a58797705dff7443de22ec0d
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-08 18:34:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,10 @@
 class Dog 
-  def name 
-    @name 
-  end 
-  
+ 
   def name=(name)
     @name = name
   end
   
-  def initialize(breed)
-    breed = @breed 
-  end 
-  
-  def breed 
-    @breed 
+  def name 
+    @name 
   end
-  
 end
